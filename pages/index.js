0402fe2402622426802af0ea296d8f7a5ec7fd93
asmitab <asmitak83@gmail.com>
2023-04-13 03:53:22 +0000

    fix: Add fallback API URL

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import Header from "@components/Header";
 import Footer from "@components/Footer";
 import useSWR from 'swr'
 
+const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://httpbin.org/get?x=a344fb8d-3a86-4dba-b388-f95bb9bec781';
+
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 export default function Home() {
 
   function Profile () {
-    const { data, error, isLoading } = useSWR(process.env.NEXT_PUBLIC_API_URL, fetcher)
+    const { data, error, isLoading } = useSWR(NEXT_PUBLIC_API_URL, fetcher)
   
     if (error) return <div>failed to load</div>
     if (isLoading) return <div>loading...</div>
