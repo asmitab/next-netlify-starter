c1adbd41aad490fbeb76dda29189086474d426c3
asmitab <asmitak83@gmail.com>
2023-04-13 03:49:08 +0000

    feat: Fetch data from Lambda backend

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
 import Head from "next/head";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
+import useSWR from 'swr'
+
+const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 export default function Home() {
+
+  function Profile () {
+    const { data, error, isLoading } = useSWR(process.env.NEXT_PUBLIC_API_URL, fetcher)
+  
+    if (error) return <div>failed to load</div>
+    if (isLoading) return <div>loading...</div>
+  
+    // render data
+    return <pre>{JSON.stringify(data, null, 2)}</pre>
+  }
+
   return (
     <div className="container">
       <Head>
@@ -21,6 +35,11 @@ export default function Home() {
           <br />
         </p>
       </main>
+
+      <pre>
+        data here
+        <Profile/>
+      </pre>
 
       <Footer />
     </div>
