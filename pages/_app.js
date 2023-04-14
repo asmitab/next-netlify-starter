a67d4e4c9fda96c0f2d1d15f5b87df4ce555cbdb
asmitab <asmitak83@gmail.com>
2023-04-14 00:41:46 +0000

    fix: Enable X-Ray again

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ function Application({ Component, pageProps }) {
       telemetries: [
         "errors",
         "performance",
-        "http",
+        ["http", { addXRayTraceIdHeader: true } ]
       ],
       allowCookies: true,
       enableXRay: true,
